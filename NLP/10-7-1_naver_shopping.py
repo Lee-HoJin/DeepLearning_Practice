3c9fe7875034603f7350ca618376de0dbbdd1a0c
pyunghak <hojin0627@unist.ac.kr>
2025-03-25 18:38:47 +0900

    25.03.25 study

---
+++ NLP/10-7-1_naver_shopping.py
@@ -0,0 +1,123 @@
+import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import urllib.request
+from collections import Counter
+from konlpy.tag import Mecab
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+urllib.request.urlretrieve("https://raw.githubusercontent.com/bab2min/corpus/master/sentiment/naver_shopping.txt", filename="ratings_total.txt")
+
+total_data = pd.read_table('ratings_total.txt', names=['ratings', 'reviews'])
+
+total_data['label'] = np.select([total_data.ratings > 3], [1], default=0)
+
+total_data.drop_duplicates(subset=['reviews'], inplace=True) # reviews 열에서 중복인 내용이 있다면 중복 제거
+
+train_data, test_data = train_test_split(total_data, test_size = 0.25, random_state = 42)
+print('훈련용 리뷰의 개수 :', len(train_data))
+print('테스트용 리뷰의 개수 :', len(test_data))
+
+# 한글과 공백을 제외하고 모두 제거
+train_data['reviews'] = train_data['reviews'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","", regex=True)
+train_data['reviews'].replace('', np.nan, inplace=True)
+
+test_data.drop_duplicates(subset = ['reviews'], inplace=True) # 중복 제거
+test_data['reviews'] = test_data['reviews'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","", regex=True) # 정규 표현식 수행
+test_data['reviews'].replace('', np.nan, inplace=True) # 공백은 Null 값으로 변경
+test_data = test_data.dropna(how='any') # Null 값 제거
+
+mecab = Mecab()
+
+stopwords = ['도', '는', '다', '의', '가', '이', '은', '한', '에', '하', '고', '을', '를', '인', '듯', '과', '와', '네', '들', '듯', '지', '임', '게']
+
+train_data['tokenized'] = train_data['reviews'].apply(mecab.morphs)
+train_data['tokenized'] = train_data['tokenized'].apply(lambda x: [item for item in x if item not in stopwords])
+
+test_data['tokenized'] = test_data['reviews'].apply(mecab.morphs)
+test_data['tokenized'] = test_data['tokenized'].apply(lambda x: [item for item in x if item not in stopwords])
+
+fig,(ax1,ax2) = plt.subplots(1,2,figsize=(10,5))
+text_len = train_data[train_data['label']==1]['tokenized'].map(lambda x: len(x))
+ax1.hist(text_len, color='red')
+ax1.set_title('Positive Reviews')
+ax1.set_xlabel('length of samples')
+ax1.set_ylabel('number of samples')
+print('긍정 리뷰의 평균 길이 :', np.mean(text_len))
+
+text_len = train_data[train_data['label']==0]['tokenized'].map(lambda x: len(x))
+ax2.hist(text_len, color='blue')
+ax2.set_title('Negative Reviews')
+fig.suptitle('Words in texts')
+ax2.set_xlabel('length of samples')
+ax2.set_ylabel('number of samples')
+print('부정 리뷰의 평균 길이 :', np.mean(text_len))
+plt.show()
+
+
+X_train = train_data['tokenized'].values
+y_train = train_data['label'].values
+X_test= test_data['tokenized'].values
+y_test = test_data['label'].values
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+threshold = 2
+total_cnt = len(tokenizer.word_index) # 단어의 수
+rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+print('단어 집합(vocabulary)의 크기 :',total_cnt)
+print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1, rare_cnt))
+print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
+print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
+
+# 전체 단어 개수 중 빈도수 2이하인 단어 개수는 제거.
+# 0번 패딩 토큰과 1번 OOV 토큰을 고려하여 +2
+vocab_size = total_cnt - rare_cnt + 2
+
+tokenizer = Tokenizer(vocab_size, oov_token = 'OOV') 
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+max_len = 80
+
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+from tensorflow.keras.layers import Embedding, Dense, GRU
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+embedding_dim = 100
+hidden_units = 128
+
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim))
+model.add(GRU(hidden_units))
+model.add(Dense(1, activation='sigmoid'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('10-7-1_best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=64, validation_split=0.2)
+
+loaded_model = load_model('10-7-1_best_model.h5')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
