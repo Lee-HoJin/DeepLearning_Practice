57d4605bb56ccddccf94c8acf445830c39e94aff
pyunghak <hojin0627@unist.ac.kr>
2025-03-25 19:15:44 +0900

    25.03.25 study

--- NLP/10-7-1_naver_shopping.py
+++ NLP/10-7-1_naver_shopping.py
@@ -128,15 +128,16 @@ print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]
 
 def sentiment_predict(new_sentence):
   new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]','', new_sentence)
-  new_sentence = mecab.morphs(new_sentence) # 토큰화
-  new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
-  encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
-  pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
-  score = float(loaded_model.predict(pad_new)) # 예측
+  new_sentence = mecab.morphs(new_sentence)
+  new_sentence = [word for word in new_sentence if not word in stopwords]
+  encoded = tokenizer.texts_to_sequences([new_sentence])
+  pad_new = pad_sequences(encoded, maxlen = max_len)
+
+  score = float(loaded_model.predict(pad_new))
   if(score > 0.5):
     print("{:.2f}% 확률로 긍정 리뷰입니다.".format(score * 100))
   else:
     print("{:.2f}% 확률로 부정 리뷰입니다.".format((1 - score) * 100))
 
-sentiment_predict('노잼 ..완전 재미 없음 ㅉㅉ')
-sentiment_predict('조금 어렵지만 재밌음ㅋㅋ')
+sentiment_predict('이 상품 진짜 좋아요... 저는 강추합니다. 대박')
+sentiment_predict('진짜 배송도 늦고 개짜증나네요. 뭐 이런 걸 상품이라고 만듬?')
