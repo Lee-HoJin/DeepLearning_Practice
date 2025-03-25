17f12243088ccda063a524d3197944feaf250724
pyunghak <hojin0627@unist.ac.kr>
2025-03-25 19:09:47 +0900

    25.03.25 study

--- NLP/10-8-1_steam_review.py
+++ NLP/10-8-1_steam_review.py
@@ -1,4 +1,3 @@
-import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,17 +8,13 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-urllib.request.urlretrieve("https://raw.githubusercontent.com/bab2min/corpus/master/sentiment/naver_shopping.txt", filename="ratings_total.txt")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/bab2min/corpus/master/sentiment/steam.txt", filename="steam.txt")
 
-total_data = pd.read_table('ratings_total.txt', names=['ratings', 'reviews'])
-
-total_data['label'] = np.select([total_data.ratings > 3], [1], default=0)
+total_data = pd.read_table('steam.txt', names=['label', 'reviews'])
 
 total_data.drop_duplicates(subset=['reviews'], inplace=True) # reviews 열에서 중복인 내용이 있다면 중복 제거
 
 train_data, test_data = train_test_split(total_data, test_size = 0.25, random_state = 42)
-print('훈련용 리뷰의 개수 :', len(train_data))
-print('테스트용 리뷰의 개수 :', len(test_data))
 
 # 한글과 공백을 제외하고 모두 제거
 train_data['reviews'] = train_data['reviews'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","", regex=True)
@@ -30,33 +25,14 @@ test_data['reviews'] = test_data['reviews'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣
 test_data['reviews'].replace('', np.nan, inplace=True) # 공백은 Null 값으로 변경
 test_data = test_data.dropna(how='any') # Null 값 제거
 
-mecab = Mecab()
+stopwords = ['도', '는', '다', '의', '가', '이', '은', '한', '에', '하', '고', '을', '를', '인', '듯', '과', '와', '네', '들', '듯', '지', '임', '게', '만', '게임', '겜', '되', '음', '면']
 
-stopwords = ['도', '는', '다', '의', '가', '이', '은', '한', '에', '하', '고', '을', '를', '인', '듯', '과', '와', '네', '들', '듯', '지', '임', '게']
+mecab = Mecab() 
 
 train_data['tokenized'] = train_data['reviews'].apply(mecab.morphs)
 train_data['tokenized'] = train_data['tokenized'].apply(lambda x: [item for item in x if item not in stopwords])
-
 test_data['tokenized'] = test_data['reviews'].apply(mecab.morphs)
 test_data['tokenized'] = test_data['tokenized'].apply(lambda x: [item for item in x if item not in stopwords])
-
-fig,(ax1,ax2) = plt.subplots(1,2,figsize=(10,5))
-text_len = train_data[train_data['label']==1]['tokenized'].map(lambda x: len(x))
-ax1.hist(text_len, color='red')
-ax1.set_title('Positive Reviews')
-ax1.set_xlabel('length of samples')
-ax1.set_ylabel('number of samples')
-print('긍정 리뷰의 평균 길이 :', np.mean(text_len))
-
-text_len = train_data[train_data['label']==0]['tokenized'].map(lambda x: len(x))
-ax2.hist(text_len, color='blue')
-ax2.set_title('Negative Reviews')
-fig.suptitle('Words in texts')
-ax2.set_xlabel('length of samples')
-ax2.set_ylabel('number of samples')
-print('부정 리뷰의 평균 길이 :', np.mean(text_len))
-plt.show()
-
 
 X_train = train_data['tokenized'].values
 y_train = train_data['label'].values
@@ -89,22 +65,20 @@ print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_fr
 # 전체 단어 개수 중 빈도수 2이하인 단어 개수는 제거.
 # 0번 패딩 토큰과 1번 OOV 토큰을 고려하여 +2
 vocab_size = total_cnt - rare_cnt + 2
+print('단어 집합의 크기 :',vocab_size)
 
 tokenizer = Tokenizer(vocab_size, oov_token = 'OOV') 
 tokenizer.fit_on_texts(X_train)
-# 토크나이저 저장
-with open('tokenizer.pickle', 'wb') as handle:
-    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
 X_train = tokenizer.texts_to_sequences(X_train)
 X_test = tokenizer.texts_to_sequences(X_test)
 
-max_len = 80
+max_len = 60
 
 X_train = pad_sequences(X_train, maxlen=max_len)
 X_test = pad_sequences(X_test, maxlen=max_len)
 
-from tensorflow.keras.layers import Embedding, Dense, GRU
+import re
+from tensorflow.keras.layers import Embedding, Dense, LSTM, Bidirectional
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
@@ -114,29 +88,11 @@ hidden_units = 128
 
 model = Sequential()
 model.add(Embedding(vocab_size, embedding_dim))
-model.add(GRU(hidden_units))
+model.add(Bidirectional(LSTM(hidden_units))) # Bidirectional LSTM을 사용
 model.add(Dense(1, activation='sigmoid'))
 
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
-mc = ModelCheckpoint('10-7-1_best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=64, validation_split=0.2)
-
-loaded_model = load_model('10-7-1_best_model.h5')
-print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
-
-def sentiment_predict(new_sentence):
-  new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]','', new_sentence)
-  new_sentence = mecab.morphs(new_sentence) # 토큰화
-  new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
-  encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
-  pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
-  score = float(loaded_model.predict(pad_new)) # 예측
-  if(score > 0.5):
-    print("{:.2f}% 확률로 긍정 리뷰입니다.".format(score * 100))
-  else:
-    print("{:.2f}% 확률로 부정 리뷰입니다.".format((1 - score) * 100))
-
-sentiment_predict('노잼 ..완전 재미 없음 ㅉㅉ')
-sentiment_predict('조금 어렵지만 재밌음ㅋㅋ')
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=256, validation_split=0.2)
