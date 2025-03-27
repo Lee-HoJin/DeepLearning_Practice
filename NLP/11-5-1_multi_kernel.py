d94fa478840bd78117fbe1a9e3129895fa4b4e40
pyunghak <hojin0627@unist.ac.kr>
2025-03-27 13:36:15 +0900

    25.03.27 study

---
+++ NLP/11-5-1_multi_kernel.py
@@ -0,0 +1,143 @@
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import urllib.request
+from konlpy.tag import Okt
+from tqdm import tqdm
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Embedding, Dropout, Conv1D, GlobalMaxPooling1D, Dense, Input, Flatten, Concatenate
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import load_model
+
+
+import pickle
+
+train_data = pd.read_table('ratings_train.txt')
+test_data = pd.read_table('ratings_test.txt')
+
+# document 열의 중복 제거
+train_data.drop_duplicates(subset=['document'], inplace=True)
+
+train_data = train_data.dropna(how = 'any') # Null 값이 존재하는 행 제거
+
+# 한글과 공백을 제외하고 모두 제거
+train_data['document'] = train_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","", regex=True)
+train_data[:5]
+
+train_data['document'] = train_data['document'].str.replace('^ +', "", regex=True) # white space 데이터를 empty value로 변경
+train_data['document'].replace('', np.nan, inplace=True)
+
+train_data = train_data.dropna(how = 'any')
+
+test_data.drop_duplicates(subset = ['document'], inplace=True) # document 열에서 중복인 내용이 있다면 중복 제거
+test_data['document'] = test_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","", regex=True) # 정규 표현식 수행
+test_data['document'] = test_data['document'].str.replace('^ +', "", regex=True) # 공백은 empty 값으로 변경
+test_data['document'].replace('', np.nan, inplace=True) # 공백은 Null 값으로 변경
+test_data = test_data.dropna(how='any') # Null 값 제거
+
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+okt = Okt()
+
+X_train = []
+for sentence in tqdm(train_data['document']):
+    tokenized_sentence = okt.morphs(sentence, stem=True) # 토큰화
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords] # 불용어 제거
+    X_train.append(stopwords_removed_sentence)
+
+X_test = []
+for sentence in tqdm(test_data['document']):
+    tokenized_sentence = okt.morphs(sentence, stem=True) # 토큰화
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords] # 불용어 제거
+    X_test.append(stopwords_removed_sentence)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+# 토크나이저 저장
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+threshold = 3
+total_cnt = len(tokenizer.word_index) # 단어의 수
+rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+print('단어 집합(vocabulary)의 크기 :',total_cnt)
+print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1, rare_cnt))
+print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
+print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
+
+# 전체 단어 개수 중 빈도수 2이하인 단어는 제거.
+# 0번 패딩 토큰을 고려하여 + 1
+vocab_size = total_cnt - rare_cnt + 1
+print('단어 집합의 크기 :',vocab_size)
+
+tokenizer = Tokenizer(vocab_size) 
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+y_train = np.array(train_data['label'])
+y_test = np.array(test_data['label'])
+
+drop_train = [index for index, sentence in enumerate(X_train) if len(sentence) < 1]
+
+max_len = 30
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+# 빈 샘플들을 제거
+X_train = np.delete(X_train, drop_train, axis=0)
+y_train = np.delete(y_train, drop_train, axis=0)
+
+
+embedding_dim = 128
+dropout_ratio = (0.5, 0.8)
+num_filters = 128
+hidden_units = 128
+
+model_input = Input(shape = (max_len,))
+z = Embedding(vocab_size, embedding_dim, input_length = max_len, name="embedding")(model_input)
+z = Dropout(dropout_ratio[0])(z)
+conv_blocks = []
+
+for sz in [3, 4, 5]:
+    conv = Conv1D(filters = num_filters,
+                         kernel_size = sz,
+                         padding = "valid",
+                         activation = "relu",
+                         strides = 1)(z)
+    conv = GlobalMaxPooling1D()(conv)
+    conv_blocks.append(conv)
+
+z = Concatenate()(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
+z = Dropout(dropout_ratio[1])(z)
+z = Dense(hidden_units, activation="relu")(z)
+model_output = Dense(1, activation="sigmoid")(z)
+
+model = Model(model_input, model_output)
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["acc"])
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('CNN_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.2, verbose=2, callbacks=[es, mc])
+
+print(model.summary())
+
+loaded_model = load_model('CNN_model.h5')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
