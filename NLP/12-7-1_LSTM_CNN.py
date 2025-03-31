59e616618b6f038f86e190fce76fdc50e9c5a617
pyunghak <hojin0627@unist.ac.kr>
2025-03-31 20:15:14 +0900

    25.03.31 study

---
+++ NLP/12-7-1_LSTM_CNN.py
@@ -0,0 +1,227 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import urllib.request
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from seqeval.metrics import f1_score, classification_report
+
+# urllib.request.urlretrieve("https://raw.githubusercontent.com/ukairia777/tensorflow-nlp-tutorial/main/12.%20Sequence%20Labeling/dataset/ner_dataset.csv", filename="ner_dataset.csv")
+data = pd.read_csv("ner_dataset.csv", encoding="latin1")
+
+print('Tag 열의 각각의 값의 개수 카운트')
+print('================================')
+print(data.groupby('Tag').size().reset_index(name='count'))
+
+data = data.fillna(method="ffill")
+
+data['Word'] = data['Word'].str.lower()
+print(data[:5])
+
+func = lambda temp: [(w, t) for w, t in zip(temp["Word"].values.tolist(), temp["Tag"].values.tolist())]
+tagged_sentences=[t for t in data.groupby("Sentence #").apply(func)]
+print("전체 샘플 개수: {}".format(len(tagged_sentences)))
+
+print(tagged_sentences[0]) # 첫번째 샘플 출력
+
+sentences, ner_tags = [], [] 
+for tagged_sentence in tagged_sentences: # 47,959개의 문장 샘플을 1개씩 불러온다.
+
+    # 각 샘플에서 단어들은 sentence에 개체명 태깅 정보들은 tag_info에 저장.
+    sentence, tag_info = zip(*tagged_sentence) 
+    sentences.append(list(sentence)) # 각 샘플에서 단어 정보만 저장한다.
+    ner_tags.append(list(tag_info)) # 각 샘플에서 개체명 태깅 정보만 저장한다.
+
+# 모든 단어를 사용하며 인덱스 1에는 단어 'OOV'를 할당.
+src_tokenizer = Tokenizer(oov_token='OOV')
+# 태깅 정보들은 내부적으로 대문자를 유지한 채 저장
+tar_tokenizer = Tokenizer(lower=False)
+
+src_tokenizer.fit_on_texts(sentences)
+tar_tokenizer.fit_on_texts(ner_tags)
+
+vocab_size = len(src_tokenizer.word_index) + 1
+tag_size = len(tar_tokenizer.word_index) + 1
+print('단어 집합의 크기 : {}'.format(vocab_size))
+print('개체명 태깅 정보 집합의 크기 : {}'.format(tag_size))
+
+# 정수 인코딩
+X_data = src_tokenizer.texts_to_sequences(sentences)
+y_data = tar_tokenizer.texts_to_sequences(ner_tags)
+
+word_to_index = src_tokenizer.word_index
+index_to_word = src_tokenizer.index_word
+ner_to_index = tar_tokenizer.word_index
+index_to_ner = tar_tokenizer.index_word
+index_to_ner[0] = 'PAD'
+
+print(index_to_ner)
+
+# 디코딩
+decoded = []
+for index in X_data[0] : # 첫번째 샘플 안의 인덱스들에 대해서
+    decoded.append(index_to_word[index]) # 다시 단어로 변환
+
+# print('기존의 문장 : {}'.format(sentences[0]))
+# print('디코딩 문장 : {}'.format(decoded))
+
+max_len = 70
+X_data = pad_sequences(X_data, padding='post', maxlen=max_len)
+y_data = pad_sequences(y_data, padding='post', maxlen=max_len)
+
+X_train, X_test, y_train_int, y_test_int = train_test_split(X_data, y_data, test_size=.2, random_state=777)
+
+y_train = to_categorical(y_train_int, num_classes=tag_size)
+y_test = to_categorical(y_test_int, num_classes=tag_size)
+
+# char_vocab 만들기
+words = list(set(data["Word"].values))
+chars = set([w_i for w in words for w_i in w])
+chars = sorted(list(chars))
+print('문자 집합 :',chars)
+
+char_to_index = {c: i + 2 for i, c in enumerate(chars)}
+char_to_index["OOV"] = 1
+char_to_index["PAD"] = 0
+
+index_to_char = {}
+for key, value in char_to_index.items():
+    index_to_char[value] = key
+
+max_len_char = 15
+
+# 문자 시퀀스에 대한 패딩하는 함수
+def padding_char_indice(char_indice, max_len_char):
+  return pad_sequences(
+        char_indice, maxlen=max_len_char, padding='post', value = 0)
+
+# 각 단어를 문자 시퀀스로 변환 후 패딩 진행
+def integer_coding(sentences):
+  char_data = []
+  for ts in sentences:
+    word_indice = [word_to_index[t] for t in ts]
+    char_indice = [[char_to_index[char] for char in t]  
+                                          for t in ts]
+    char_indice = padding_char_indice(char_indice, max_len_char)
+
+    for chars_of_token in char_indice:
+      if len(chars_of_token) > max_len_char:
+        continue
+    char_data.append(char_indice)
+  return char_data
+
+# 문자 단위 정수 인코딩 결과
+X_char_data = integer_coding(sentences)
+
+X_char_data = pad_sequences(X_char_data, maxlen=max_len, padding='post', value = 0)
+
+X_char_train, X_char_test, _, _ = train_test_split(X_char_data, y_data, test_size=.2, random_state=777)
+
+X_char_train = np.array(X_char_train)
+X_char_test = np.array(X_char_test)
+
+def sequences_to_tag(sequences):
+    result = []
+    # 전체 시퀀스로부터 시퀀스를 하나씩 꺼낸다.
+    for sequence in sequences:
+        word_sequence = []
+        # 시퀀스로부터 확률 벡터 또는 원-핫 벡터를 하나씩 꺼낸다.
+        for pred in sequence:
+            # 정수로 변환. 예를 들어 pred가 [0, 0, 1, 0 ,0]라면 1의 인덱스인 2를 리턴한다.
+            pred_index = np.argmax(pred)            
+            # index_to_ner을 사용하여 정수를 태깅 정보로 변환. 'PAD'는 'O'로 변경.
+            word_sequence.append(index_to_ner[pred_index].replace("PAD", "O"))
+        result.append(word_sequence)
+    return result
+
+def sequences_to_tag_for_crf(sequences): 
+    result = []
+    # 전체 시퀀스로부터 시퀀스를 하나씩 꺼낸다.
+    for sequence in sequences: 
+        word_sequence = []
+        # 시퀀스로부터 예측 정수 레이블을 하나씩 꺼낸다.
+        for pred_index in sequence:
+            # index_to_ner을 사용하여 정수를 태깅 정보로 변환. 'PAD'는 'O'로 변경.
+            word_sequence.append(index_to_ner[pred_index].replace("PAD", "O"))
+        result.append(word_sequence)
+    return result
+
+# print('훈련 샘플 문장의 크기 : {}'.format(X_train.shape))
+# print('훈련 샘플 레이블(정수 인코딩)의 크기 : {}'.format(y_train_int.shape))
+# print('훈련 샘플 레이블(원-핫 인코딩)의 크기 : {}'.format(y_train.shape))
+# print('테스트 샘플 문장의 크기 : {}'.format(X_test.shape))
+# print('테스트 샘플 레이블(정수 인코딩)의 크기 : {}'.format(y_test_int.shape))
+# print('테스트 샘플 레이블(원-핫 인코딩)의 크기 : {}'.format(y_test.shape))
+
+import tensorflow as tf
+from tensorflow.keras.layers import Embedding, Input, TimeDistributed, Dropout, concatenate, Bidirectional, LSTM, Conv1D, Dense, MaxPooling1D, Flatten
+from tensorflow.keras import Model
+from tensorflow.keras.initializers import RandomUniform
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import load_model
+from seqeval.metrics import f1_score, classification_report
+from keras_crf import CRFModel
+
+embedding_dim = 128
+char_embedding_dim = 64
+dropout_ratio = 0.5
+hidden_units = 256
+num_filters = 30
+kernel_size = 3
+
+# 단어 임베딩
+word_ids = Input(shape=(None,),dtype='int32', name='words_input')
+word_embeddings = Embedding(input_dim=vocab_size, output_dim=embedding_dim)(word_ids)
+
+# char 임베딩
+char_ids = Input(shape=(None, max_len_char,), name='char_input')
+embed_char_out = TimeDistributed(Embedding(len(char_to_index), char_embedding_dim, embeddings_initializer=RandomUniform(minval=-0.5, maxval=0.5)), name='char_embedding')(char_ids)
+dropout = Dropout(dropout_ratio)(embed_char_out)
+
+# char 임베딩에 대해서는 Conv1D 수행
+conv1d_out = TimeDistributed(Conv1D(kernel_size=kernel_size, filters=num_filters, padding='same', activation='tanh', strides=1))(dropout)
+maxpool_out = TimeDistributed(MaxPooling1D(max_len_char))(conv1d_out)
+char_embeddings = TimeDistributed(Flatten())(maxpool_out)
+char_embeddings = Dropout(dropout_ratio)(char_embeddings)
+
+# char 임베딩을 Conv1D 수행한 뒤에 단어 임베딩과 연결
+output = concatenate([word_embeddings, char_embeddings])
+
+# 연결한 벡터를 가지고 문장의 길이만큼 LSTM을 수행
+output = Bidirectional(LSTM(hidden_units, return_sequences=True, dropout=dropout_ratio))(output)
+
+# 출력층
+output = TimeDistributed(Dense(tag_size, activation='softmax'))(output)
+
+model = Model(inputs=[word_ids, char_ids], outputs=[output])
+model.compile(loss='categorical_crossentropy', optimizer='nadam',  metrics=['acc'])
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('bilstm_cnn.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+history = model.fit([X_train, X_char_train], y_train, batch_size=128, epochs=15, validation_split=0.1, verbose=1, callbacks=[es, mc])
+
+model = load_model('bilstm_cnn.h5')
+
+i = 13 # 확인하고 싶은 테스트용 샘플의 인덱스.
+# 입력한 테스트용 샘플에 대해서 예측 y를 리턴
+y_predicted = model.predict([np.array([X_test[i]]), np.array([X_char_test[i]])])
+
+y_predicted = np.argmax(y_predicted, axis=-1) # 확률 벡터를 정수 인코딩으로 변경.
+labels = np.argmax(y_test[i], -1) # 원-핫 인코딩을 정수 인코딩으로 변경.
+
+print("{:15}|{:5}|{}".format("단어", "실제값", "예측값"))
+print(35 * "-")
+
+for word, tag, pred in zip(X_test[i], labels, y_predicted[0]):
+    if word != 0: # PAD값은 제외함.
+        print("{:17}: {:7} {}".format(index_to_word[word], index_to_ner[tag], index_to_ner[pred]))
+
+y_predicted = model.predict([X_test, X_char_test])
+pred_tags = sequences_to_tag(y_predicted)
+test_tags = sequences_to_tag(y_test)
+
+print("F1-score: {:.1%}".format(f1_score(test_tags, pred_tags)))
+print(classification_report(test_tags, pred_tags))
