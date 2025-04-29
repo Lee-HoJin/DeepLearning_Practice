cf6ef98a2930df0aef08feaf724846bec361e682
pyunghak <hojin0627@unist.ac.kr>
2025-04-29 14:09:10 +0900

    25.04.29 study

---
+++ NLP/17-2-1_WordPiece.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from transformers import BertTokenizer
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased") # Bert-base의 토크나이저
+
+result = tokenizer.tokenize('Here is the sentence I want embeddings for.')
+print(result)
+
+# BERT의 단어 집합을 vocabulary.txt에 저장
+with open('vocabulary.txt', 'w') as f:
+  for token in tokenizer.vocab.keys():
+    f.write(token + '\n')
+
+df = pd.read_fwf('vocabulary.txt', header=None)
+print(df)
+
