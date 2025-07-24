b65a7646577d5fb3234b0e1786817cf50da55384
pyunghak <hojin0627@unist.ac.kr>
2025-07-24 15:48:39 +0900

    25.07.24 full version update

--- mastering_transformers/7-1-ner-finetuning.py
+++ mastering_transformers/7-1-ner-finetuning.py
@@ -1,10 +1,18 @@
 import datasets
-from transformers import DistilBertTokenizerFast
+import numpy as np 
+import json
+from transformers import DistilBertTokenizerFast, \
+                         AutoModelForTokenClassification, \
+                         TrainingArguments, \
+                         Trainer,  \
+                         DataCollatorForTokenClassification, \
+                         pipeline
+
 
 conll2003 = datasets.load_dataset("conll2003")
 
-print(conll2003["train"][0])
-print(conll2003["train"].features["ner_tags"])
+# print(conll2003["train"][0])
+# print(conll2003["train"].features["ner_tags"])
 
 tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
 
@@ -42,8 +50,86 @@ def tokenize_and_align_labels(examples) :
 
     return tokenized_inputs
 
+# q = tokenize_and_align_labels(conll2003['train'][4:5])
 
-q = tokenize_and_align_labels(conll2003['train'][4:5])
+# for token, label in zip( tokenizer.convert_ids_to_tokens( q["input_ids"][0]), q["labels"][0]):
+#     print(f"{token:_<40} {label}")
 
-for token, label in zip( tokenizer.convert_ids_to_tokens( q["input_ids"][0]), q["labels"][0]):
-    print(f"{token:_<40} {label}")
+tokenized_datasets = conll2003.map(tokenize_and_align_labels, batched = True)
+
+model = AutoModelForTokenClassification.from_pretrained("distilbert-base-uncased", num_labels = 9)
+args = TrainingArguments(
+    "test-ner",
+    evaluation_strategy = "epoch",
+    learning_rate = 2e-5,
+    per_device_training_batch_size = 16,
+    per_device_eval_batch_size = 16,
+    num_train_epochs = 3,
+    weight_decay = 0.01
+)
+
+data_collator =  DataCollatorForTokenClassification(tokenizer)
+
+metric = datasets.load_metric("seqeval")
+
+example = conll2003['train'][0]
+label_list = conll2003["train"].features["ner_tags"].feature.names
+labels = [label_list[i] for i in example["ner_tags"]]
+metric.compute(predictions=[labels], references=[labels])
+
+
+def compute_metrics(p): 
+    predictions, labels = p 
+    predictions = np.argmax(predictions, axis=2) 
+    true_predictions = [ 
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100] 
+        for prediction, label in zip(predictions, labels) 
+    ] 
+    true_labels = [ 
+      [label_list[l] for (p, l) in zip(prediction, label) if l != -100] 
+       for prediction, label in zip(predictions, labels) 
+   ] 
+    results = metric.compute(predictions=true_predictions, references=true_labels) 
+    return { 
+   "precision": results["overall_precision"], 
+   "recall": results["overall_recall"], 
+   "f1": results["overall_f1"], 
+  "accuracy": results["overall_accuracy"], 
+  } 
+
+trainer = Trainer( 
+    model, 
+    args, 
+   train_dataset=tokenized_datasets["train"], 
+   eval_dataset=tokenized_datasets["validation"], 
+   data_collator=data_collator, 
+   tokenizer=tokenizer, 
+   compute_metrics=compute_metrics 
+) 
+
+trainer.train() 
+
+model.save_pretrained("ner_model")
+tokenizer.save_pretrained("tokenizer")
+
+id2label = {
+    str(i): label for i,label in enumerate(label_list)
+}
+label2id = {
+    label: str(i) for i,label in enumerate(label_list)
+}
+
+
+config = json.load(open("ner_model/config.json"))
+config["id2label"] = id2label
+config["label2id"] = label2id
+
+json.dump(config, open("ner_model/config.json","w"))
+
+mmodel = AutoModelForTokenClassification.from_pretrained("ner_model")
+
+nlp = pipeline("ner", model=mmodel, tokenizer=tokenizer)
+example = "I live in Istanbul"
+
+ner_results = nlp(example)
+print(ner_results)
