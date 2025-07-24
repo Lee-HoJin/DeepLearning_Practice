2c1352874f1655601491250576eaff9ddc7c637b
pyunghak <hojin0627@unist.ac.kr>
2025-07-24 14:42:43 +0900

    25.07.24 preproc added

--- mastering_transformers/7-1-ner-finetuning.py
+++ mastering_transformers/7-1-ner-finetuning.py
@@ -1,9 +1,46 @@
 import datasets
 from transformers import DistilBertTokenizerFast
 
-conll2003 = datasets.load_dataset("conll2003")
+def main() :
 
-print(conll2003["train"][0])
-print(conll2003["train"].features["ner_tags"])
+    conll2003 = datasets.load_dataset("conll2003")
 
-tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
+    print(conll2003["train"][0])
+    print(conll2003["train"].features["ner_tags"])
+
+    tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
+
+    def tokenize_and_align_labels(examples) :
+        tokenized_inputs = tokenizer(examples["tokens"],
+                                     truncation = True,
+                                     is_split_into_words = True)
+        
+        labels = []
+
+        for i, label in enumerate(examples["ner_tags"]) :
+            word_ids = tokenized_inputs.word_ids(batch_index = i)
+            previous_word_idx = None
+            label_ids = []
+
+            for word_idx in word_ids :
+                if word_idx is None :
+                    label_ids.append(-100)
+
+                elif word_idx != previous_word_idx :
+                    label_ids.append(label[word_idx])
+
+                else :
+                    label_ids.append(label[word_idx] if label_all_tokens else -100)
+
+                previous_word_idx = word_idx
+
+            labels.append(label_ids)
+
+        tokenized_inputs["labels"] = labels
+
+        return tokenized_inputs
+    
+
+    print(tokenize_and_align_labels(conll2003['train'][4:5]))
+
+    return 0
